1025dbb9a0474d782e31bf9c22bba161a78ce72e
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-06-25 04:09:47 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,25 +3,24 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * Return: 1 for positive number, -1 for negative number or 0 for anything else
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 	_putchar(43);
 	return (1);
-	} 
+	}
 	else if (n < 0)
 	{
 	_putchar(45);
 	return (-1);
 	}
-	else 
+	else
 	{
 	_putchar(48);
-        return (0);
+       return (0);
 	}
 }
