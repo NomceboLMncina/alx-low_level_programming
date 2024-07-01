8ec420a062d76a88f97c22e7a20068c37792f82f
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-07-01 02:16:12 -0700

    7. Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - Prints the last half of a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len;
+
+	len = _strlen(str);
+	i = len / 2;
+
+	if (len % 2 != 0)
+		i = (len - 1) / 2;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
