b7c4bd32b7541787c80e06b92a4813a60d78d83a
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-10-05 16:40:07 -0700

    Task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function to create a hash table
+ * @size: Size of the array
+ * Return: Null for error or a pointer to a new hash table
+ * else, pointer to new table
+ *
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int x;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (x = 0; x < size; ++x)
+	{
+		ht->array[x] = NULL;
+	}
+	return (ht);
+}
